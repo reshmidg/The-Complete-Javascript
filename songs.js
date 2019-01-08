973dde90a8a186bdfc5527ec4e144ae94b44d70c
reshmi <reshmi.datta35@gmail.com>
2019-01-08 17:42:20 +0530

    "Javascript Homework #1

---
+++ songs.js
@@ -0,0 +1,40 @@
+/* This file contains a 
+String, 
+Number, 
+Boolean, 
+Array and 
+an Object attribute for 
+Tere Liye song */
+
+// Song name : String
+var name = 'Tere Liye';
+
+// Duration of the song : Number
+var durationInSeconds = 180;
+
+// Singers for the song : Array
+var singers = ['Lata Mangeshka','Roop Kumar Rathod'];
+
+// Bollywood category : Boolean
+var isBollywood = true;
+
+// Song Details : Object
+var songDetails = {
+	movieName : 'Veer Zara',
+	musicDirector : 'Madan Mohan'
+}
+
+// Outputs the name
+console.log(name);
+
+// Outputs the Duration of the song
+console.log(durationInSeconds);
+
+// Outputs the Singers for the song
+console.log(singers);
+
+// Outputs the Bollywood category
+console.log(isBollywood);
+
+// Outputs the Song Details
+console.log(songDetails);
